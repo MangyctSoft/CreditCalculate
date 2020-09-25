ff93114f219ea93a672a3a9cfe43afd1df1b0107
Mangyct <andrei-df@mail.ru>
2020-09-25 14:08:39 +0300

    JS для выбора типа ставки.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    const stepPayment = $("#stepPayment");
+
+    $("#changeStacks").change((stacks) => {
+        if (stacks.currentTarget.value == 1) {
+            stepPayment.addClass("visually--hidden")
+        }       
+        if (stacks.currentTarget.value == 2) {
+            stepPayment.removeClass("visually--hidden")
+        } 
+    })
+
+   
+    
+});
